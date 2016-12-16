002a3cd183bab1d05e439b67a9e5597665f4af9d
Daniel Choi <dhchoi@gmail.com>
2016-12-16 12:00:13 -0500

    copy in example

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,29 @@
+{-# LANGUAGE OverloadedStrings, RecordWildCards, ScopedTypeVariables, ViewPatterns #-} 
 module Main where
+import Network.WebSockets
+import qualified Data.ByteString.Lazy as LBS
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  runServer "127.0.0.1" 8080 handleConnection
+
+
+parseWant :: LBS.ByteString -> Maybe Text
+parseWant = T.stripPrefix "I want " . T.decodeUtf8 . LBS.toStrict
+
+handleConnection pending = do
+  connection <- acceptRequest pending
+  let loop wants = do
+          commandMsg <- receiveDataMessage connection
+          case commandMsg of
+            Text (parseWant -> Just want) -> do
+              sendTextData connection
+                ("Hohoho, as long as you've been good this year!" :: Text)
+              loop (want : wants)
+            _ -> do
+              sendTextData connection ("<img src=\"http://bit.ly/1kmRC7Q\" />" :: Text)
+              loop wants
+  loop []
