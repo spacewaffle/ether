29bedc5b118749e68e74de9ddcca0bb98fdbcffe
Daniel Choi <dhchoi@gmail.com>
2016-12-16 12:00:49 -0500

    indent

--- src/Main.hs
+++ src/Main.hs
@@ -17,13 +17,14 @@ parseWant = T.stripPrefix "I want " . T.decodeUtf8 . LBS.toStrict
 handleConnection pending = do
   connection <- acceptRequest pending
   let loop wants = do
-          commandMsg <- receiveDataMessage connection
-          case commandMsg of
-            Text (parseWant -> Just want) -> do
-              sendTextData connection
-                ("Hohoho, as long as you've been good this year!" :: Text)
-              loop (want : wants)
-            _ -> do
-              sendTextData connection ("<img src=\"http://bit.ly/1kmRC7Q\" />" :: Text)
-              loop wants
+        commandMsg <- receiveDataMessage connection
+        case commandMsg of
+          Text (parseWant -> Just want) -> do
+            sendTextData connection
+              ("Hohoho, as long as you've been good this year!" :: Text)
+            loop (want : wants)
+          _ -> do
+            sendTextData connection ("<img src=\"http://bit.ly/1kmRC7Q\" />" :: Text)
+            loop wants
   loop []
+
