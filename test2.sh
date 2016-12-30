f63341ea5c10f0dd97d07e7d1b6eb48602009453
Daniel Choi <dhchoi@gmail.com>
2016-12-30 17:57:36 -0500

    test2.sh throttle script

---
+++ test2.sh
@@ -0,0 +1,11 @@
+
+for x in $(seq 1 100) ; do
+  echo $x
+  curl http://localhost:8081/sse?chan=a &
+done
+
+for x in $(seq 1 100) ; do
+  echo $x
+  curl -d '{"body":"one","chan":"a","name":"dan0","type":"chat_message"}' http://localhost:8081/message
+  sleep 0.05
+done
