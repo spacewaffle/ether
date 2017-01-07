6a4b01f79469ce3e4e61458c0852662a643730d8
Jon Cheng <jon.syndicate@gmail.com>
2017-01-07 02:21:01 -0500

    switch to username instead of email field

--- ether_app/app/controllers/chats_controller.rb
+++ ether_app/app/controllers/chats_controller.rb
@@ -3,6 +3,6 @@ class ChatsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @username = current_user.email
+    @username = current_user.username
   end
 end
