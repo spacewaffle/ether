0fa6a0588c3f0d555e6398135e3ba333f68d316f
Daniel Choi <dhchoi@gmail.com>
2017-01-11 10:42:05 -0500

    don't use records for more compactness

--- haskell/User.hs
+++ haskell/User.hs
@@ -1,6 +1,40 @@
+{-# LANGUAGE OverloadedStrings, RecordWildCards, ScopedTypeVariables #-} 
 module User where
 import Database.PostgreSQL.Simple
 import Data.Text (Text)
+import Data.Int (Int64)
+import Data.Maybe 
+import Control.Applicative
+
+data User = 
+    User 
+      Int64 -- id
+      Text -- username
+      Text -- email 
 
 
+getUserById :: Connection -> Int64 -> IO (Maybe User)
+getUserById c uid = 
+    query c "select id, username, email from users where id = ?" (Only uid)
+    >>= \xs -> 
+          case xs of 
+              (i, u, e):_ -> return $ Just $ User i u e
+              _ -> return Nothing
+
+
+------------------------------------------------------------------------
+data UserCreate = 
+    UserCreate 
+        Text  -- username 
+        Text  -- email
+
+createUser :: Connection -> UserCreate -> IO User
+createUser c (UserCreate u e) = do
+    x <- query c "insert into users (username, email) values (?, ?) returning id" (u, e)
+    -- may get username uniqueness exception
+    case x of
+      (Only uid):_ -> fromJust <$> getUserById c uid
+      _ -> error ("Failed to create user: " ++ show u)
+
+------------------------------------------------------------------------
 
